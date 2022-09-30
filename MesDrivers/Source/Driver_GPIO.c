1b9c0e5549d68e4c3ec032c6dbca98027bedcd64
Ghizlane Badaoui <badaou@insa-toulouse.fr>
2022-09-30 14:30:46 +0200

    add Driver GPIO

--- MesDrivers/Source/Driver_GPIO.c
+++ MesDrivers/Source/Driver_GPIO.c
@@ -1,27 +1,45 @@
+#include <stdio.h>
 #include "stm32f10x.h"
 #include "Driver_GPIO.h"
 
-
-//RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
-//RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN;
-
 void MyGPIO_Init ( MyGPIO_Struct_TypeDef * GPIOStructPtr) {
-	if (GPIOStructPtr->GPIO==GPIOA) RCC->APB2ENR |= RCC_APB2ENR_IOPAEN ;
-	else if (GPIOStructPtr->GPIO==GPIOB) RCC->APB2ENR |= RCC_APB2ENR_IOPBEN ;
-	else if (GPIOStructPtr->GPIO==GPIOC) RCC->APB2ENR |= RCC_APB2ENR_IOPCEN ;
-	
-	if (GPIOStructPtr->GPIO_Pin >= '0' && GPIOStructPtr->GPIO_Pin <= '7' ){
-		
-			
+	if (GPIOStructPtr->GPIO==GPIOA) {
+		RCC->APB2ENR |= RCC_APB2ENR_IOPAEN ;
 	}
-	else
-		
+	else if (GPIOStructPtr->GPIO==GPIOB) {
+		RCC->APB2ENR |= RCC_APB2ENR_IOPBEN ;
+	}
+	else if (GPIOStructPtr->GPIO==GPIOC) {
+		RCC->APB2ENR |= RCC_APB2ENR_IOPCEN ;
+	}
 	
+	if ((int)GPIOStructPtr->GPIO_Pin >= 0 && (int)GPIOStructPtr->GPIO_Pin <= 7 ){
+		GPIOStructPtr->GPIO->CRL &= ~(0xF << 4*(int)GPIOStructPtr->GPIO_Pin);
+		GPIOStructPtr->GPIO->CRL |= GPIOStructPtr->GPIO_Conf << 4*(int)GPIOStructPtr->GPIO_Pin;
+	}
+	
+	if ((int)GPIOStructPtr->GPIO_Pin >= 8 && (int)GPIOStructPtr->GPIO_Pin <= 15 ){
+		GPIOStructPtr->GPIO->CRH &= ~(0xF << 4*((int)GPIOStructPtr->GPIO_Pin -8));
+		GPIOStructPtr->GPIO->CRH |= GPIOStructPtr->GPIO_Conf << 4*((int)GPIOStructPtr->GPIO_Pin -8);
+	}
+
 }
+
 
 int MyGPIO_Read ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-	if (GPIO_Pin >= '0' && GPIO_Pin <= '7' )
-			GPIO->CRL = 
-	else
-			
+	if (GPIO->IDR & (1 << GPIO_Pin)) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
+
+void MyGPIO_Set ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
+	GPIO->ODR &= ~(1 << GPIO_Pin);
+}
+
+void MyGPIO_Reset ( GPIO_TypeDef * GPIO , char GPIO_Pin ) {
+	GPIO->ODR |= (1 << GPIO_Pin);
+}
+
+
--- Projet1/Source/Principal.c
+++ Projet1/Source/Principal.c
@@ -1,18 +1,11 @@
 #include "stm32f10x.h"
 #include "Driver_GPIO.h"
 
-
-
-
+/*
 int main (void)
 {
 	//RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN;
-	
-	
-
-	
-	
 	
 	//PC10 : LED
 	GPIOC->CRH &= ~(0xF << 8);
@@ -23,12 +16,42 @@ int main (void)
 	GPIOC->CRH |= 4;
 	
 	while(1){
-		
 		if (GPIOC->IDR & (1 << 8)) {
 			GPIOC->ODR &= ~(1 << 10);
 		}else {
 			GPIOC->ODR |= (1 << 10);
 		}
-		
+	}
+}
+*/
+
+int main (void)
+{	
+	//gpioStruct_1 for the LED and gpioStruct_2 for the button
+	MyGPIO_Struct_TypeDef gpioStruct_1, gpioStruct_2;
+	
+	//PC10 : LED
+	gpioStruct_1.GPIO = GPIOC;
+	gpioStruct_1.GPIO_Conf = Out_Ppull;
+	gpioStruct_1.GPIO_Pin = 10;
+	
+	// clock enable for LED GPIO
+	MyGPIO_Init(&gpioStruct_1);
+	
+	
+	//PC8 : BUTTON	
+	gpioStruct_2.GPIO = GPIOC;
+	gpioStruct_2.GPIO_Conf = In_Floating;
+	gpioStruct_2.GPIO_Pin = 8;
+	
+	// clock enable for BUTTON GPIO
+	MyGPIO_Init(&gpioStruct_2);
+	
+	while (1){
+		if (MyGPIO_Read ( gpioStruct_2.GPIO, gpioStruct_2.GPIO_Pin )) {
+				MyGPIO_Set ( gpioStruct_1.GPIO , gpioStruct_1.GPIO_Pin );
+		}else {
+				MyGPIO_Reset ( gpioStruct_1.GPIO , gpioStruct_1.GPIO_Pin );
+		}
 	}
 }
